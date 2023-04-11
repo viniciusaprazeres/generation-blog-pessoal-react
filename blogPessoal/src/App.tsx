c550fdb6aee96d9038e5f61686333c484ab4f0f7
viniciusaprazeres <viniciusaprazeres@gmail.com>
2023-04-11 12:06:51 -0300

    Criação do componente Navbar

--- blogPessoal/src/App.tsx
+++ blogPessoal/src/App.tsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './components/navbar/Navbar.css'
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Navbar></Navbar>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
