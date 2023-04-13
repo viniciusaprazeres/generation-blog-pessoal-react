50b48244bd41cb8a81f9314bc92410b7b7f239ac
viniciusaprazeres <viniciusaprazeres@gmail.com>
2023-04-13 14:32:25 -0300

    Criação da página de Login

--- blogPessoal/src/App.tsx
+++ blogPessoal/src/App.tsx
@@ -2,20 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import './components/navbar/Navbar.css'
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer'
-import Home from './components/home/Home';
+import './components/estaticos/navbar/Navbar.css'
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer'
+import Home from './paginas/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './paginas/login/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <Home />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/home' element={<Home />} />
+            {/* <Route path='/cadastro' element={<CadastroUsuario />} /> */}
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
--- blogPessoal/src/paginas/login/Login.tsx
+++ blogPessoal/src/paginas/login/Login.tsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import Container from '@mui/material/Container';
+import { Button, Grid, TextField, Typography } from '@material-ui/core';
+import { Box } from '@mui/material';
+import { Link } from 'react-router-dom'
+
+import './Login.css'
 
 function Login() {
   return (
     <>
-    Login
+      <Grid container direction='row' justifyContent='center' alignItems='center'>
+        <Grid xs={6} justifyContent='center' alignItems='center'>
+          <Box paddingX={20}>
+            <form>
+              <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' style={{ fontWeight: 'bold'}}>Entrar</Typography>
+              <TextField id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
+              <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+              <Box marginTop={2} textAlign='center'>
+                <Link to='/home' className='text-decoration-none'>
+                  <Button type='submit' variant='contained' color='primary'>
+                    Logar
+                  </Button>
+                </Link>
+              </Box>
+            </form>
+            <Box display={'flex'} justifyContent={'center'} marginTop={2}>
+              <Box marginRight={1}>
+                <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
+              </Box>
+                <Typography variant='subtitle1' gutterBottom align='center' style={{ fontWeight: 'bold'}}>Cadastre-se</Typography>
+            </Box>
+          </Box>
+        </Grid>
+
+        <Grid xs={6} className="imagemLogin">
+        </Grid>
+      
+      </Grid>
     </>
   )
 }
