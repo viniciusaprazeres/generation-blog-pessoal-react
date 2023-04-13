3f36ca6e4df22bd854ac0c7ecbd2a57bccf3f060
viniciusaprazeres <viniciusaprazeres@gmail.com>
2023-04-13 15:22:33 -0300

    Refatoração do CSS dos componentes e páginas

--- blogPessoal/src/components/estaticos/navbar/Navbar.tsx
+++ blogPessoal/src/components/estaticos/navbar/Navbar.tsx
@@ -45,36 +45,36 @@ function Navbar() {
       <AppBar position="static">
           <Toolbar variant="dense">
               <Box display={'flex'} justifyContent={'space-between'} width={'100%'}>
-                <Box style={{ cursor: "pointer" }} >
+                <Box className='cursor'>
                   <Typography variant="h5" color="inherit">
                       BlogPessoal
                   </Typography>
               </Box>
 
               <Box display="flex" justifyContent="start">
-                  <Box mx={1} style={{ cursor: "pointer" }}>
+                  <Box mx={1} className='cursor'>
                       <Typography variant="h6" color="inherit">
                           home
                       </Typography>
                   </Box>
-                  <Box mx={1} style={{ cursor: "pointer" }}>
+                  <Box mx={1} className='cursor'>
                       <Typography variant="h6" color="inherit">
                           postagens
                       </Typography>
                   </Box>
-                  <Box mx={1} style={{ cursor: "pointer" }}>
+                  <Box mx={1} className='cursor'>
                       <Typography variant="h6" color="inherit">
                           temas
                       </Typography>
                   </Box>
-                  <Box mx={1} style={{ cursor: "pointer" }}>
+                  <Box mx={1} className='cursor'>
                       <Typography variant="h6" color="inherit">
                           cadastrar tema
                       </Typography>
                   </Box>
                   <Link to='/login' className='text-decoration-none'>
-                      <Box mx={1} style={{ cursor: "pointer", color: "white" }}>
-                          <Typography variant="h6" color="inherit">
+                      <Box mx={1} className='cursor'>
+                          <Typography variant="h6">
                               logout
                           </Typography>
                       </Box>
--- blogPessoal/src/paginas/login/Login.tsx
+++ blogPessoal/src/paginas/login/Login.tsx
@@ -13,7 +13,7 @@ function Login() {
         <Grid xs={6} justifyContent='center' alignItems='center'>
           <Box paddingX={20}>
             <form>
-              <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' style={{ fontWeight: 'bold'}}>Entrar</Typography>
+              <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textoLogin'>Entrar</Typography>
               <TextField id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
               <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
               <Box marginTop={2} textAlign='center'>
@@ -28,7 +28,7 @@ function Login() {
               <Box marginRight={1}>
                 <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
               </Box>
-                <Typography variant='subtitle1' gutterBottom align='center' style={{ fontWeight: 'bold'}}>Cadastre-se</Typography>
+                <Typography variant='subtitle1' gutterBottom align='center' className='textoLogin'>Cadastre-se</Typography>
             </Box>
           </Box>
         </Grid>
