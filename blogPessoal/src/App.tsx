2c6b9a2072183c26ed11ddc7f9953288b7d5f067
Vinicius Prazeres <viniciusaprazeres@gmail.com>
2023-04-17 21:32:26 -0300

    Criação do componente de cadastro de usuário Modificações na tela de login e implementação da página de cadastro de usuário.

--- blogPessoal/src/App.tsx
+++ blogPessoal/src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import './components/estaticos/navbar/Navbar.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer'
 import Home from './paginas/home/Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './paginas/login/Login'
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+
+import './App.css'
+import './components/estaticos/navbar/Navbar.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,7 +22,7 @@ function App() {
             <Route path='/' element={<Login />} />
             <Route path='/login' element={<Login />} />
             <Route path='/home' element={<Home />} />
-            {/* <Route path='/cadastro' element={<CadastroUsuario />} /> */}
+            <Route path='/cadastrousuario' element={<CadastroUsuario />} />
         </Routes>
       </div>
       <Footer />
--- blogPessoal/src/paginas/login/Login.tsx
+++ blogPessoal/src/paginas/login/Login.tsx
@@ -28,7 +28,9 @@ function Login() {
               <Box marginRight={1}>
                 <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
               </Box>
-                <Typography variant='subtitle1' gutterBottom align='center' className='textoLogin'>Cadastre-se</Typography>
+                <Link to={'/cadastrousuario'}>
+                  <Typography variant='subtitle1' gutterBottom align='center' className='textoLogin'>Cadastre-se</Typography>
+                </Link>
             </Box>
           </Box>
         </Grid>
