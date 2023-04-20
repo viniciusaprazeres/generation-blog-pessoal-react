2cab58e7a4a8018ed2d750feb4d60a17d9d23595
Vinicius Prazeres <viniciusaprazeres@gmail.com>
2023-04-20 12:07:23 -0300

    Criação das models Tema e Postagem

---
+++ blogPessoal/src/models/Postagem.ts
@@ -0,0 +1,11 @@
+import { Tema } from "./Tema";
+
+export interface Postagem {
+    
+    id: number,
+    titulo: string,
+    texto: string,
+    data: string,
+    tema: Tema
+    
+}
---
+++ blogPessoal/src/models/Tema.ts
@@ -0,0 +1,6 @@
+export interface Tema {
+
+    id: number,
+    descricao: string
+    
+}
