2c1599999d3c2932d57c80d898dfa664639904df
Vinicius Prazeres <viniciusaprazeres@gmail.com>
2023-04-25 19:24:03 -0300

    Refatoração do componente para deletar postagem

--- blogPessoal/src/components/postagens/deletaPostagem/DeletaPostagem.tsx
+++ blogPessoal/src/components/postagens/deletaPostagem/DeletaPostagem.tsx
@@ -1,50 +1,108 @@
-import React, { useEffect, useState }  from "react";
-import {Typography, Button, Card, CardActions, CardContent } from "@material-ui/core"
-import {Box} from '@mui/material';
+import React, { useEffect, useState } from "react";
+import {
+  Grid,
+  Typography,
+  Button,
+  Card,
+  Box,
+  CardActions,
+  CardContent,
+} from "@mui/material";
+import { useNavigate, useParams } from "react-router-dom";
+import useLocalStorage from "react-use-localstorage";
+import { useSelector } from "react-redux";
 
+import { Tema } from "../../../models/Tema";
 import { Postagem } from "../../../models/Postagem";
+import { TokenState } from "../../../store/tokens/tokensReducer";
 
-import { getId, deleteId } from '../../../services/Service';
+import { getId, deleteId } from "../../../services/Service";
 
-import './DeletaPostagem.css'
+import "./DeletaPostagem.css";
 
-function deletaPostagem() {
-    
+function DeletaPostagem() {
+
+  const history = useNavigate();
+  
+  const token = useSelector<TokenState, TokenState["token"]>(
+    (state) => state.token
+  );
+  
+  const { id } = useParams<{ id: string }>();
+
+  const [postagem, setPostagem] = useState<Postagem>();
+
+  useEffect(() => {
+    if (token === "") {
+      alert("Você precisa estar logado!");
+      history("/login");
+    }
+  }, []);
+
+  async function getPostagemById(id: string) {
+    await getId(`/postagens/${id}`, setPostagem, {
+      headers: {
+        Authorization: token,
+      },
+    });
+  }
+
+  useEffect(() => {
+    if (id !== undefined) {
+      getPostagemById(id);
+    }
+  });
+
+  function deletarPostagem() {
+    deleteId(`/postagens/${id}`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+    alert("Postagem deletada com sucesso.");
+    history("/postagens");
+  }
+
+  function voltar() {
+    history("/postagens");
+  }
+
   return (
     <>
-      <Box m={2}>
-        <Card variant="outlined">
-          <CardContent>
-            <Box justifyContent="center">
-              <Typography color="textSecondary" gutterBottom>
-                Deseja deletar a Postagem:
-              </Typography>
-              <Typography color="textSecondary">Tema</Typography>
+      <Grid container justifyContent={"center"} mt={4}>
+        <Grid item xs={4}>
+          <Card variant="outlined">
+            <Typography variant="h3" gutterBottom align="center">
+              Deletar Postagem
+            </Typography>
+            <Typography variant="body1" gutterBottom align="center">
+              Você tem certeza de que deseja deletar a postagem com título:{" "}
+              <br /> <strong>{postagem?.titulo}</strong>{" "}
+            </Typography>
+
+            <Box display="flex">
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={voltar}
+                fullWidth
+              >
+                Não
+              </Button>
+              <Button
+                variant="contained"
+                color="error"
+                onClick={deletarPostagem}
+                fullWidth
+              >
+                Sim
+              </Button>
             </Box>
-          </CardContent>
-          <CardActions>
-            <Box display="flex" justifyContent="start" ml={1.0} mb={2}>
-              <Box mx={2}>
-                <Button
-                  variant="contained"
-                  className="marginLeft"
-                  size="large"
-                  color="primary"
-                >
-                  Sim
-                </Button>
-              </Box>
-              <Box>
-                <Button variant="contained" size="large" color="secondary">
-                  Não
-                </Button>
-              </Box>
-            </Box>
-          </CardActions>
-        </Card>
-      </Box>
+          </Card>
+        </Grid>
+      </Grid>
     </>
   );
 }
 
-export default deletaPostagem;
+export default DeletaPostagem;
